a695763050dd0c8d3df15c9f98ebf22b3ff2b39c
Thiago Malaquias <tammalaquias@gmail.com>
2020-11-01 22:11:47 -0300

    fix/ create alunos

--- app/controllers/alunos_controller.rb
+++ app/controllers/alunos_controller.rb
@@ -25,15 +25,36 @@ class AlunosController < ApplicationController
   # POST /alunos
   # POST /alunos.json
   def create
-    # if request.path_parameters[:format] == 'json'
-    #   aluno = Aluno.new
-    #   aluno.callback_type = params[:callback_type]
-    #   aluno.save!
-
-    #   render json: aluno.to_json, status: 200
-    # end
-    
-    @aluno = Aluno.new(aluno_params)
+    if request.path_parameters[:format] == 'json'
+      @aluno = Aluno.new
+      @aluno.callback_type = params[:callback_type]
+      @aluno.hottok = params[:hottok]
+      @aluno.aff = params[:aff]
+      @aluno.aff_name = params[:aff_name]
+      @aluno.currency = params[:currency]
+      @aluno.xcode = params[:xcode]
+      @aluno.payment_type = params[:payment_type]
+      @aluno.status = params[:status]
+      @aluno.prod = params[:prod]
+      @aluno.prod_name = params[:prod_name]
+      @aluno.producer_name = params[:producer_name]
+      @aluno.purchase_date = params[:purchase_date]
+      @aluno.original_offer_price = params[:original_offer_price]
+      @aluno.confirmation_purchase_date = params[:confirmation_purchase_date]
+      @aluno.cms_marketplace = params[:cms_marketplace]
+      @aluno.cms_vendor = params[:cms_vendor]
+      @aluno.price = params[:price]
+      @aluno.full_price = params[:full_price]
+      @aluno.name_subscription_plan = params[:name_subscription_plan]
+      @aluno.email = params[:email]
+      @aluno.name = params[:name]
+      @aluno.first_name = params[:first_name]
+      @aluno.last_name = params[:last_name]
+      @aluno.phone_checkout_local_code = params[:phone_checkout_local_code]
+      @aluno.phone_checkout_number = params[:phone_checkout_number]
+    else
+      @aluno = Aluno.new(aluno_params)
+    end
     
     respond_to do |format|
       if @aluno.save
