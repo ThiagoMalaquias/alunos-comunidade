ec7e6f068653ca6d5fa4cb405d2389308ec65783
Thiago Malaquias <tammalaquias@gmail.com>
2021-02-02 16:26:12 -0300

    new layout adm

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
     @alunos = Aluno.all.order("name asc")
+    @total_alunos = Aluno.count
+    @alunos_ativos = Aluno.where(status: "approved").count
+    @alunos_cancelados = Aluno.where(status: "canceled").count
   end
 
   def elements; end
