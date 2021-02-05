ef5ef85cc80e36a5c8de20678990cc6e9bae7e9e
Thiago Malaquias <tammalaquias@gmail.com>
2021-02-05 02:53:32 -0300

    Home index

--- app/helpers/alunos_helper.rb
+++ app/helpers/alunos_helper.rb
@@ -3,6 +3,10 @@ module AlunosHelper
   def tipo_pagamento(payment_type)
     if payment_type == "credit_card"
       return "Cartão de Crédito"
+
+    elsif payment_type == "billet_printed"
+      return "Boleto Gerado"
+
     else
       return "Boleto"
     end
@@ -11,8 +15,18 @@ module AlunosHelper
   def status_pagamento_aluno(status)
     if status == "approved"
       return "Aprovado"
-    else
+
+    elsif status == "refunded"
+      return "Reembolsado"
+    
+    elsif status == "delayed"
+      return "Atrasado"
+      
+    elsif status == "completed"
       return "Cancelado"
+
+    else
+      return "billet_printed, dispute, completed, blocked, chargeback, expired, wayting_payment"
     end
   end
 end
